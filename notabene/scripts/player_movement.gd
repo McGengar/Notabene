4dff8417c1bd3795a8b5fc4fcdd26376511ba84c
McGengar <macwegrzyn@gmail.com>
2024-11-30 00:29:59 +0100

    can now swing at both sides

--- notabene/scripts/player_movement.gd
+++ notabene/scripts/player_movement.gd
@@ -8,26 +8,34 @@ var can_attack = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite2D2.visible = false
+	$pivot/AnimatedSprite2D2.visible = false
 	$AnimatedSprite2D.animation = "stand"
-	$AnimatedSprite2D2.play()
+	$pivot/AnimatedSprite2D2.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	character_direction.x = Input.get_axis("move_left","move_right")
 	character_direction.y = Input.get_axis("move_up","move_down")
 	character_direction = character_direction.normalized()
-
+	
+	if character_direction.x>0:
+		$pivot.scale.x=1
+	if character_direction.x<0:
+		$pivot.scale.x=-1
 	if Input.is_action_just_pressed("attack") and can_attack:
+		if character_direction.x<0:
+			$AnimatedSprite2D.flip_h = true
 		is_attacking = true
 		can_attack = false
-		$AnimatedSprite2D2.visible= true
+		$pivot/AnimatedSprite2D2.visible= true
 		$AnimatedSprite2D.animation = "attack"
-		$AnimatedSprite2D2.play()
+		$pivot/AnimatedSprite2D2.play()
 		await get_tree().create_timer(0.25).timeout
+		$AnimatedSprite2D.flip_h = false
 		is_attacking = false
 		await get_tree().create_timer(0.25).timeout
 		can_attack = true
+		
 		
 	if is_attacking ==false:
 		if character_direction.x!=0 or character_direction.y!=0:
