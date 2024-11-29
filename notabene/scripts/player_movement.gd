16f5fabc1741fa1aefe63445f86c200a3bb4ac50
McGengar <macwegrzyn@gmail.com>
2024-11-29 20:23:25 +0100

    player spirite and script

---
+++ notabene/scripts/player_movement.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var character_direction : Vector2
+var movement_speed = 100.0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed('move_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('move_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('move_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('move_up'):
+		velocity.y -= 1
+		
+		velocity = velocity*movement_speed
+		move_and_slide()
