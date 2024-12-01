ec0d71ddfe41002a63f4417c60b30a543d33ee45
McGengar <macwegrzyn@gmail.com>
2024-12-01 12:15:58 +0100

    changes and home

--- notabene/scripts/office.gd
+++ notabene/scripts/office.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var time =0
-
+var zoomin = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Node2D/Player.can_move =false
@@ -9,7 +9,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	if time>1:
+	if time>1 and zoomin==false:
 		$Node2D/Player/Camera2D.zoom.x= lerpf($Node2D/Player/Camera2D.zoom.x, 4, 2*delta)
 		$Node2D/Player/Camera2D.zoom.y= lerpf($Node2D/Player/Camera2D.zoom.y, 4, 2*delta)
 	if time==4:
@@ -17,17 +17,21 @@ func _physics_process(delta):
 	
 	if time>5 and $Node2D/Player/Camera2D/AnimatedSprite2D.frame == 1:
 		$Node2D/Player/Camera2D/AnimatedSprite2D.visible=false
+	if zoomin == true:
+		$Node2D/Player.camera_shake(0.5)
+		if $Node2D/Player/Camera2D.zoom.x<16:
+			$Node2D/Player/Camera2D.zoom.x*=1.003
+			$Node2D/Player/Camera2D.zoom.y*=1.003
+		if $Node2D/Player/Camera2D.zoom.x>=16:
+			$Node2D/Player/Camera2D/Sprite2D2.modulate = Color(0,0,0,255)
+			await get_tree().create_timer(1).timeout
+			get_tree().change_scene_to_packed(load("res://scenes/boss_fight.tscn"))
 
 func _on_timer_timeout():
 	time+=1
 
 func camera_zoom():
-	while 	$Node2D/Player/Camera2D.zoom.x<7:
-			$Node2D/Player/Camera2D.zoom.x*=1.0007
-			$Node2D/Player/Camera2D.zoom.y*=1.0007
-	$Node2D/Player/Camera2D/Sprite2D2.modulate = Color(0,0,0,255)
-	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_packed(load("res://scenes/boss_fight.tscn"))
+	zoomin = true
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
