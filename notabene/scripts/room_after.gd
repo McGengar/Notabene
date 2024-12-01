7939699c6688e3185f3999e26514005af6d18747
skr8it <unrankedop@gmail.com>
2024-12-01 15:13:37 +0100

    ending dialogue

--- notabene/scripts/room_after.gd
+++ notabene/scripts/room_after.gd
@@ -5,8 +5,10 @@ extends Node2D
 func _ready():
 	await get_tree().create_timer(10).timeout
 	$Node2D/Player/Camera2D/Sprite2D2.modulate = Color(0,0,0,255)
-
-
+	DialogueManager.show_dialogue_balloon(load("res://dialogue/enddialogue.dialogue"), "start")
+func exit():
+	await get_tree().create_timer(1.5).timeout
+	get_tree().change_scene_to_packed(load("res://scenes/main.tscn"))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
