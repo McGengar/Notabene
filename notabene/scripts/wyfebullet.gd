19e5ccb82e213cba7c7e079e16c2cf6d2ccb07a5
McGengar <macwegrzyn@gmail.com>
2024-12-01 12:52:38 +0100

    wyfe fight

--- notabene/scripts/wyfebullet.gd
+++ notabene/scripts/wyfebullet.gd
@@ -12,6 +12,9 @@ func _physics_process(delta):
 	apply_central_force(player_direction*movement_speed*1000*delta)
 
 
-func _on_area_2d_body_entered(body: RigidBody2D) -> void:
+
+
+
+func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		body.take_dmg(20)
