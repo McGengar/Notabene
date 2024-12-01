2f5f462c992fc7eb7fdca0a42da49b2563bcc4bb
skr8it <unrankedop@gmail.com>
2024-12-01 13:14:16 +0100

    menubuttons etc

---
+++ notabene/scripts/startbutton.gd
@@ -0,0 +1,20 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_packed(load("res://scenes/metro.tscn"))
+
+
+func _on_texture_button_2_pressed() -> void:
+	await get_tree().create_timer(1).timeout
+	get_tree().quit()
